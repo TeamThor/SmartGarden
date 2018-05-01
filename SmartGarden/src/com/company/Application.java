0f926cc62493949e496ff2acfc5c1fec8da8bf9e
Tearez <martinreff1@gmail.com>
2018-05-01 19:09:49 +0300

    add a plant menu added

--- SmartGarden/src/com/company/Application.java
+++ SmartGarden/src/com/company/Application.java
@@ -9,8 +9,8 @@ import java.util.HashSet;
 import java.util.Scanner;
 
 public class Application {
-    HashSet<String> gardensNames;
-    ArrayList<Garden> gardens;
+    private HashSet<String> gardensNames;
+    private ArrayList<Garden> gardens;
     public Application(){
         gardensNames = new HashSet<>();
         gardens = new ArrayList<>();
@@ -73,11 +73,16 @@ public class Application {
                 case 2:
                     removeGardenMenu();
                     break;
-                case 3:break;
+                case 3:
+                    addPlantMenu();
+                    break;
                 case 4:
                     listAllGardens();
                     break;
                 case 5:
+                    break;
+                default:
+                    System.out.println("Not a valid choice");
                     break;
             }
         }while (choice!=5);
@@ -113,6 +118,9 @@ public class Application {
                         e.getMessage();
                     }
                 case 5: break;
+                default:
+                    System.out.println("Not a valid choice");
+                    break;
             }
         }while (choice!=5);
     }
@@ -149,6 +157,44 @@ public class Application {
                 System.out.println(gardens.get(choice-1) + " has been removed.");
                 gardens.remove(choice-1);
             }
+        }
+    }
+
+    private void addPlantMenu(){
+        Scanner in = new Scanner(System.in);
+        String[] optionsList = {"Add a:\n",
+                "1. Deciduous tree\n",
+                "2. Coniferous tree\n",
+                "3. Single fruit plant\n",
+                "4. Multiple fruit plant\n",
+                "5. Vegetable",
+                "6. Exit\n\n"};
+
+        while (true){
+            System.out.print("Enter your choice -> ");
+            int choice = Integer.parseInt(in.nextLine());
+            System.out.println();
+
+            switch (choice){
+                case 1:
+                    System.out.print("Enter the gardens name ->");
+                    String gardenName = in.nextLine();
+                    System.out.print("Enter the trees name ->");
+                    String treeName = in.nextLine();
+                    System.out.print("Enter the planting date(DD.MM.YYYY) ->");
+                    String date = in.nextLine();
+                    addDeciduousTree(gardenName,treeName,date,1,TreeHeight.AVEARAGE);
+                    break;
+                case 2:break;
+                case 3:break;
+                case 4:break;
+                case 5:break;
+                case 6:break;
+                default:
+                    System.out.println("Not a valid choice");
+                    break;
+            }
+
         }
     }
 }
